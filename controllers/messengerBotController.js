5789570888d6e1f7d85b149bc31eebd459f47244
railsjack <jinnahrae@gmail.com>
2019-07-17 19:13:58 -0400

    Added a logic to answer the user's name

--- controllers/messengerBotController.js
+++ controllers/messengerBotController.js
@@ -1,32 +1,44 @@
 exports.testDb = async (req, res) => {
     try {
-      const client = await pool.connect()
-      const result = await client.query('SELECT * FROM test_table');
-      const results = { 'results': (result) ? result.rows : null};
-      res.render('pages/db', results );
-      client.release();
+        const client = await pool.connect()
+        const result = await client.query('SELECT * FROM test_table')
+        const results = { 'results': (result) ? result.rows : null }
+        res.render('pages/db', results)
+        client.release()
     } catch (err) {
-      console.error(err);
-      res.send("Error " + err);
+        console.error(err)
+        res.send("Error " + err)
     }
-  }
+}
 
 exports.messengerLoad = async (req, res) => {
     try {
-      res.render('pages/messenger_bot' );
+        res.render('pages/messenger_bot')
     } catch (err) {
-      console.error(err);
-      res.send("Error " + err);
+        console.error(err)
+        res.send("Error " + err)
     }
-  }
+}
 
 exports.handleMessage = async (req, res) => {
     try {
-      console.log('message: '+JSON.stringify(req.body));
-      res.render('pages/messenger_bot' );
+        const message_type = req.body.message_type
+        console.log('message: ' + message_type)
+
+        let response;
+        switch ('message_type') {
+            case 'start':
+                const name = req.body.message
+                response = "Hi\nWhat's your name?"
+                break;
+            case 'ANSWERING_NAME':
+                response = "Ok, great! "
+                break;
+        }
+        res.send({ response: response })
     } catch (err) {
-      console.error(err);
-      res.send("Error " + err);
+        console.error(err);
+        res.send("Error " + err)
     }
-  }
+}
 
--- public/messenger-bot/scripts.js
+++ public/messenger-bot/scripts.js
@@ -30,25 +30,74 @@ var MB = (function(){
     };
     var _boatWindow = function(){return _bind(document.querySelector('.messenger-bot'))}
     var _boatForm = function(){return _bind(document.querySelector('.messenger-bot form'))}
+    var _messageBox = function(){return _bind(document.querySelector('.messenger-bot .content .messages'))}
     var _toggle = function(){
         var _win = _boatWindow();
         _win.toggleClass('expand');
         if (_win.hasClass('expand')){
-            _startChat();
+            if(!onceStarted)_startChat();
         }
     };
+    var onceStarted = false;
+    var messageStatus = '';
     var _startChat = function(){ 
+        onceStarted = true;
         ajax.post('/messenger-bot-message', 
         {
             dataType: 'json',
-            data: {message: 'start'}
+            data: {message_type:'start', message: ''}
         }
-        , function(response){
-            console.log('response: ' + response);
-        })
-     };
+        , function(xhr){
+            var xhr = JSON.parse(xhr);
+            var messageNode = document.createElement('li');
+            messageNode.innerHTML = '<span class="bot">'+xhr.response+'</span>';
+            _messageBox().appendChild(messageNode);
+            messageStatus = 'ASKING_NAME';
+        });
+    };
+
+    var _handleSubmit = function(event){
+        var message = this.text.value;
+        var messageNode = document.createElement('li');
+        messageNode.innerHTML = '<span class="me">'+message+'</span>';
+        _messageBox().appendChild(messageNode);
+
+        switch(messageStatus){
+            case 'ASKING_NAME':
+                    ajax.post('/messenger-bot-message', 
+                    {
+                        dataType: 'json',
+                        data: {message_type: 'ANSWER_NAME', message: message}
+                    }
+                    , function(xhr){
+                        var xhr = JSON.parse(xhr);
+                        var messageNode = document.createElement('li');
+                        messageNode.innerHTML = '<span class="bot">'+xhr.response+'</span>';
+                        _messageBox().appendChild(messageNode);
+                        messageStatus = 'ASKING_BIRTHDAY';
+                    });
+            
+                break;
+            case 'ASKING_BIRTHDAY':
+                    ajax.post('/messenger-bot-message', 
+                    {
+                        dataType: 'json',
+                        data: {message_type: 'ANSWER_BIRTHDAY', message: message}
+                    }
+                    , function(xhr){
+                        var xhr = JSON.parse(xhr);
+                        var messageNode = document.createElement('li');
+                        messageNode.innerHTML = '<span class="bot">'+xhr.response+'</span>';
+                        _messageBox().appendChild(messageNode);
+                        messageStatus = 'ASKING_BIRTHDAY';
+                    });
+                break;
+        }
+
+        event.preventDefault(); return false; 
+    };
     var _init = function(){
-        _boatForm().bindEvents('submit', function(event){ event.preventDefault(); return false; });
+        _boatForm().bindEvents('submit', _handleSubmit);
     };
     return {
         toggle: _toggle,
